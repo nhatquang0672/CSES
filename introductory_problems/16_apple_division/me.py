bb929c78a376d2a04ad1dbe318fefb002d9f6c4a
quangtn3 <quangtn3@vng.com.vn>
2021-05-28 09:44:27 +0700

    introductory

---
+++ introductory_problems/16_apple_division/me.py
@@ -0,0 +1,30 @@
+
+
+# def apple_division(n, arr):
+#     total_sum = sum(arr)
+#     d = total_sum
+
+#     for i in range(1<<n):
+#         tmp_sum = 0
+#         for j in range(n, -1, -1):
+#             if 1<<j&i != 0:
+#                 tmp_sum += arr[j]
+#         d = min(d, abs(total_sum - 2*tmp_sum))
+
+#     print(d)
+# apple_division(n, arr)
+def apple_division_recursion(n, arr):
+    ans = float('inf')
+    def dfs(a, b):
+        if a == n:
+            ans = min(ans, abs(b))
+        dfs(a+1, b+arr[a])
+        dfs(a+1, b-arr[a])
+        
+    dfs(0,0)
+    print(ans)
+
+n = int(input())
+arr = [int(x) for x in input().split(' ')]
+apple_division_recursion(n, arr)
+
