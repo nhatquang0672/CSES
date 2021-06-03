38639848914deed0e94df4b6278560649b9410c5
quangtn3 <quangtn3@vng.com.vn>
2021-06-03 18:29:48 +0700

    playlist

--- sorting_and_searching/12_collecting_numbers_2_unsolve/me.py
+++ sorting_and_searching/12_collecting_numbers_2_unsolve/me.py
@@ -1,10 +1,6 @@
 # https://cses.fi/problemset/task/2217
 
-def collecting_numbers(n, arr):
-    xx = [0]*(n+1)
-    for i in range(n):
-        xx[arr[i]] = i
-
+def collecting_numbers(n, xx):
     ans, last = 1, 0
     for i in range(1, n+1):
         if last > xx[i]:
@@ -14,7 +10,13 @@ def collecting_numbers(n, arr):
 
 n, m = map(int, input().split(' '))
 arr = [int(x) for x in input().split(' ')]
+
+xx = [0]*(n+1)
+for i in range(n):
+    xx[arr[i]] = i
+    
 for i in range(m):
     a, b = map(int, input().split(' '))
     arr[a-1] , arr[b-1] = arr[b-1], arr[a-1]
-    collecting_numbers(n, arr)
+    xx[arr[a-1]], xx[arr[b-1]] = xx[arr[b-1]], xx[arr[a-1]]
+    collecting_numbers(n, xx)
